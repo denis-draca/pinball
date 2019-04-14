0403c698f76fac4a7674b411406bcf0b411e1be5
Denis Draca <denis.draca@gmail.com>
2019-04-14 15:04:29 +1000

    Defined an object

--- headers/object.h
+++ headers/object.h
@@ -1,11 +1,43 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <QImage>
 
 class object
 {
 public:
     object();
+
+public:
+    //setters
+    void setSprite(const char *imgLoc);
+
+    //getters
+    double getVelocityX()   {return m_velocityX;}
+    double getVelocityY()   {return m_velocityY;}
+    double getaccelX()      {return m_accelX;}
+    double getaccelY()      {return m_accelY;}
+    double getGravity()     {return m_gravity;}
+
+    QImage getSprite()      {return m_sprite;}
+
+private://Members
+    QImage m_sprite;
+
+    QPoint m_location;
+
+    double m_velocityX;
+    double m_velocityY;
+
+    double m_accelX;
+    double m_accelY;
+
+    const double m_gravity = 4.0;
+
+    bool m_fixed;
+
+private://Methods
+    void init();
 };
 
 #endif // OBJECT_H
--- src/object.cpp
+++ src/object.cpp
@@ -2,5 +2,22 @@
 
 object::object()
 {
+    init();
+}
 
+void object::setSprite(const char *imgLoc)
+{
+    m_sprite = QImage(imgLoc);
+}
+
+void object::init()
+{
+    m_fixed     = false;
+    m_velocityX = 0;
+    m_velocityY = 0;
+    m_accelX    = 0;
+    m_accelY    = 0;
+
+    m_location.setX(0);
+    m_location.setY(0);
 }
